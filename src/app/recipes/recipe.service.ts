f4f2a27aa578661c342a486e0319d8015d380880
mkanderka <matthew.kanderka@westjet.com>
2018-08-09 11:31:57 -0600

    first

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Test', 'Test', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg', [new Ingredient('Meat',1)]),
+        new Recipe('Test2', 'Test2', '', [new Ingredient('Chicken',1)])
+      ];
+      
+      constructor(private slService: ShoppingListService) { }
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+      }
+}
